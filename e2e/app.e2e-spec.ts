007895ee823ba7bf1a7976cd2507ffb4127e1088
Sergey Penkovsky <dev@erlymon.org>
2017-09-27 22:47:02 +0300

    upgraded angular 2 template

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ErlymonWebPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('erlymon-web App', () => {
-  let page: ErlymonWebPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ErlymonWebPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
