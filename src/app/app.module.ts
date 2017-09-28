ce03dea72aff7afcb810b5f18685a123dbba51fe
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2017-09-28 17:06:50 +0300

    added i18n lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,6 +25,8 @@ import {
 } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core"; //изменилось
+import { TranslateHttpLoader } from "@ngx-translate/http-loader"; //изменилось
 
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
@@ -31,6 +34,11 @@ import { SignInPageComponent } from './components/sign-in-page/sign-in-page.comp
 import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 
 // определение маршрутов
@@ -55,6 +63,7 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes, { enableTracing: false }),
     BrowserAnimationsModule,
     MdButtonModule,
@@ -73,7 +82,14 @@ const appRoutes: Routes = [
     MdDialogModule,
     MdSelectModule,
     CdkTableModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
