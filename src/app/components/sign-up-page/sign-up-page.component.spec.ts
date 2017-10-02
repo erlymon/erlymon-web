9e8e89c38086a73f0e043dc667641fc6d8586f16
Sergey Penkovsky <sergey.penkovsky@gmail.com>
2017-10-02 17:29:36 +0300

    added license

--- src/app/components/sign-up-page/sign-up-page.component.spec.ts
+++ src/app/components/sign-up-page/sign-up-page.component.spec.ts
@@ -1,3 +1,22 @@
+/**
+    Erlymon is an open source GPS tracking system for various GPS tracking devices.
+
+    Copyright (C) 2015, Sergey Penkovsky <sergey.penkovsky@gmail.com>.
+
+    This file is part of Erlymon.
+
+    Erlymon is free software: you can redistribute it and/or  modify
+    it under the terms of the GNU Affero General Public License, version 3,
+    as published by the Free Software Foundation.
+
+    Erlymon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpPageComponent } from './sign-up-page.component';
@@ -8,9 +27,9 @@ describe('SignUpPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignUpPageComponent ]
+      declarations: [SignUpPageComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,22 @@
+/**
+    Erlymon is an open source GPS tracking system for various GPS tracking devices.
+
+    Copyright (C) 2015, Sergey Penkovsky <sergey.penkovsky@gmail.com>.
+
+    This file is part of Erlymon.
+
+    Erlymon is free software: you can redistribute it and/or  modify
+    it under the terms of the GNU Affero General Public License, version 3,
+    as published by the Free Software Foundation.
+
+    Erlymon is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/long-stack-trace-zone';
@@ -17,7 +36,7 @@ declare const __karma__: any;
 declare const require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function() { };
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
